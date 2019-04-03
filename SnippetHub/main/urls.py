f293ef95b4a4b83cda9367b36c2c1cb895fb7319
Ritvik19 <rastogiritvik99@gmail.com>
2019-04-03 22:10:13 +0530

    main app

--- SnippetHub/main/urls.py
+++ SnippetHub/main/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from . import views
+from django.urls import path
+
+app_name = 'main'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('main.urls'))
+    path('', views.homepage, name='homepage')
 ]
