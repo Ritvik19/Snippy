5db09c20c59089de4fbbb00bfb22e9f08af29d1d
Ritvik19 <rastogiritvik99@gmail.com>
2019-04-08 21:58:14 +0530

    Model

--- SnippetHub/main/models.py
+++ SnippetHub/main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Snippet(models.Model):
+    snippet_title = models.CharField(max_length = 200)
+    snippet_content = models.TextField()
+    snippet_published = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.snippet_title
