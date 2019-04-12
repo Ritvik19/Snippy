67a9e1f6fa1976cb74201748b25f3aaf30151d1a
Ritvik19 <rastogiritvik99@gmail.com>
2019-04-12 23:49:45 +0530

    Admin

--- SnippetHub/main/models.py
+++ SnippetHub/main/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class Snippet(models.Model):
     snippet_title = models.CharField(max_length = 200)
+    snippet_type = models.CharField(max_length = 20)
     snippet_content = models.TextField()
-    snippet_published = models.DateTimeField('date published')
+    snippet_published = models.DateTimeField('date published', default=datetime.now())
 
     def __str__(self):
-        return self.snippet_title
+        return self.snippet_title + '-' + self.snippet_type
