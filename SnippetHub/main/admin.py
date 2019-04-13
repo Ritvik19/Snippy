12ff16911719752d7f19dc0a63b08055136dfca6
Ritvik19 <rastogiritvik99@gmail.com>
2019-04-13 15:29:15 +0530

    Admin: TinyMCE

--- SnippetHub/main/admin.py
+++ SnippetHub/main/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Snippet
+from tinymce.widgets import TinyMCE
+from django.db import models
 
 # Register your models here.
+class SnippetAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Title/date", {'fields': ["snippet_title", "snippet_type", "snippet_published"]}),
+        ("Content", {"fields": ["snippet_content"]})
+    ]
+    formfield_overrides = {
+        models.TextField : {'widget': TinyMCE()}
+    }
 
-admin.site.register(Snippet)
+admin.site.register(Snippet, SnippetAdmin)
