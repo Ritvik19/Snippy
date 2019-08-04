5245b23252df6ee601ac2681a845e94d8dd0054d
Ritvik19 <rastogiritvik99@gmail.com>
2019-08-04 13:39:49 +0530

    UI

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
 });
+
+window.onscroll = function() {scrollfunc()};
+
+function scrollfunc() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+// postlist, posts(id: blog-i), mobile-demo
