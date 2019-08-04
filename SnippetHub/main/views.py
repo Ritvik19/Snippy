288521c996064810b39dc6e647e9965346cd4a62
Ritvik19 <rastogiritvik99@gmail.com>
2019-08-04 12:01:12 +0530

    Reinitiated

--- SnippetHub/main/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Snippet
-# Create your views here.
-
-def homepage(request):
-    return render(request=request,
-    template_name='main/home.html',
-    context={'snippets':Snippet.objects.all})
