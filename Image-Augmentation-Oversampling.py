b9098e420edd50961bdbfa94c774e29702e66c03
Ritvik19 <rastogiritvik99@gmail.com>
2020-06-13 21:36:33 +0530

    Snippy commit 100

---
+++ Image-Augmentation-Oversampling.py
@@ -0,0 +1,50 @@
+import os, random
+import matplotlib.pyplot as plt
+from tqdm.auto import tqdm
+from keras.preprocessing.image import ImageDataGenerator
+
+dataaug = ImageDataGenerator(
+    shear_range = 0.2, zoom_range = 0.2, horizontal_flip = True, rotation_range=10,
+    height_shift_range=0.1, width_shift_range=0.1
+)
+
+def balance_class(source_directory, target_directory, imagegen_obj, target_size=None):
+    
+    class_examples = []
+    os.mkdir(target_directory)
+    for directory in os.listdir(source_directory):
+        os.mkdir(os.path.join(target_directory, directory))
+        class_examples.append(len(os.listdir(os.path.join(source_directory, directory))))
+    
+    print('Class Distribution:', class_examples)
+    
+    if target_size == None:
+        target_size = max(class_examples)
+        
+    for i, directory in enumerate(os.listdir(source_directory)):
+        q, r = divmod(target_size, class_examples[i])
+        q, r = q-1, r/class_examples[i]
+        dest_dir = os.path.join(target_directory, directory)
+        for imgpath in tqdm(os.listdir(os.path.join(source_directory, directory))):
+            filename, file_extension = os.path.splitext(imgpath)
+            img = plt.imread(os.path.join(source_directory, directory, imgpath))
+            plt.imsave(os.path.join(dest_dir, imgpath), img)
+            for i in range(q):
+                aug_img = imagegen_obj.random_transform(img)
+                plt.imsave(os.path.join(dest_dir, f"{filename}-{i+1}{file_extension}"), aug_img)
+            prob = random.random()
+            if prob <= r:
+                aug_img = imagegen_obj.random_transform(img)
+                plt.imsave(os.path.join(dest_dir, f"{filename}-{0}{file_extension}"), aug_img)
+                
+    class_examples_updated = []
+    for directory in os.listdir(target_directory):
+        class_examples_updated.append(len(os.listdir(os.path.join(target_directory, directory))))   
+    print('Updated Class Distribution:', class_examples_updated)
+    
+    
+    
+balance_class(
+    source_directory='dataset/train/', target_directory='dataset/train-balanced/',
+    imagegen_obj=dataaug, target_size=None
+)    
