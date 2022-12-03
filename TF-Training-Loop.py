ed2ea05fbad1448de18ee286a37185d978680613
Ritvik19 <rastogiritvik99@gmail.com>
2022-12-03 14:02:14 +0530

    Snippy commit 118

--- TF-Training-Loop.py
+++ TF-Training-Loop.py
@@ -1,5 +1,13 @@
-with tf.GradientTape() as tape:
-    logits = model(X)
-    loss = loss_fn(y, logits)
-gradients = tape.gradient(loss, model.trainable_weights)
-optimizer.apply_gradients(zip(gradients, model.trainable_weights))
+loss_fn = keras.losses.SparseCategoricalCrossentropy()
+loss_tracker = keras.metrics.Mean(name="loss")
+
+def training_step(self, data):
+    X, y = data
+    with tf.GradientTape() as tape:
+        logits = self(X)
+        loss = loss_fn(y, logits)
+    gradients = tape.gradient(loss, self.trainable_weights)
+    optimizer.apply_gradients(zip(gradients, self.trainable_weights))
+
+    loss_tracker.update_state(loss)
+    return {"loss": loss_tracker.result()}
