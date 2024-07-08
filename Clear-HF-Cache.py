1d830d2d51cdb1e261bdfdf2bb55e5e8a3c5f733
Ritvik19 <rastogiritvik99@gmail.com>
2024-07-09 00:52:36 +0530

    Commit 119

---
+++ Clear-HF-Cache.py
@@ -0,0 +1,4 @@
+from transformers import TRANSFORMERS_CACHE
+import shutil
+
+shutil.rmtree(TRANSFORMERS_CACHE)
---
+++ git-lfs-aws.sh
@@ -0,0 +1,5 @@
+which amazon-linux-extras
+sudo yum install -y amazon-linux-extras
+sudo amazon-linux-extras installepel -y
+sudo yum-config-manager --enable-epel
+sudo yum install git-lfs -y
